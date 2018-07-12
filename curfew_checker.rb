6820e2477fbe8b900d33d561aaa81b663b59bc4b
Sidneyyy <catitude14@outlook.com>
2018-07-12 17:02:46 +0000

    automatically backed up by learn

---
+++ curfew_checker.rb
@@ -0,0 +1,30 @@
+def simple_curfew_checker(time)
+  if 4 < 5
+    puts "Four is less than five."
+end
+
+def curfew_checker(time)
+  if 6 < 5
+    puts "Six is less than five."
+  else
+    "Six is not less than five."
+end
+
+def complex_curfew_checker(time)
+  if 6 < 5
+    puts "Six is less than five."
+  elsif 6 == 6
+    puts "Six equals six."
+  else 
+    puts "Six is not less than five and six does not equal six."
+end
+
+def deluxe_curfew_checker(time)
+  num_of_apples = 6
+  puts "I have #{num_of_apples} apples."
+  
+end
+
+def platinum_curfew_checker(current_time, curfew_time)
+  # code goes here
+end
